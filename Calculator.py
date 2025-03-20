0c5cf797c0fb6d96b566ba7ceafe53583a7db609
Syconn <49461127+Syconn@users.noreply.github.com>
2025-03-20 02:22:34 -0400

    Solidified File

--- Calculator.py
+++ Calculator.py
@@ -1,6 +1,4 @@
 # Calculates the Solution to the Equation
-import Operations
-
 def calculate(problem: str):
     return equation(problem.split(" "))
 
@@ -12,5 +10,23 @@ def find_operation(parts: list[str]):
 def equation(parts):
     while len(parts) > 1:
         op = find_operation(parts)
-        parts = parts[0:op[0]-1] + [Operations.evaluate(parts, op[0])] + parts[op[0]+2:]
+        parts = parts[0:op[0]-1] + [evaluate(parts, op[0])] + parts[op[0]+2:]
     return parts[0]
+
+def evaluate(parts: list[str], spot: int) -> str:
+    return str(operation(parts[spot-1:spot+2]))
+
+def operation(op: list[str]) -> float | int:
+    match op[1]:
+        case "*":
+            return zero(float(op[0]) * float(op[2]))
+        case "/":
+            return zero(float(op[0]) / float(op[2]))
+        case "-":
+            return zero(float(op[0]) - float(op[2]))
+        case _:
+            return zero(float(op[0]) + float(op[2]))
+
+def zero(f: float) -> float | int:
+    if str(f).endswith('.0'): return int(f)
+    return f
